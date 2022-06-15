05fd1fafe24dd0509a3b6cccc0eec782aec045d8
Josh Justice <jjustice@justworks.com>
2022-06-15 12:41:37 -0400

    Send message

--- components/NewMessageForm.spec.js
+++ components/NewMessageForm.spec.js
@@ -3,11 +3,14 @@ import userEvent from '@testing-library/user-event';
 import NewMessageForm from './NewMessageForm';
 
 describe('<NewMessageForm />', () => {
+  let sendHandler;
+
   describe('clicking the send button', () => {
     async function sendMessage() {
       const user = userEvent.setup();
+      sendHandler = jest.fn().mockName('sendHandler');
 
-      render(<NewMessageForm />);
+      render(<NewMessageForm onSend={sendHandler} />);
 
       await user.type(screen.getByTestId('messageText'), 'New message');
       await user.click(screen.getByTestId('sendButton'));
@@ -16,6 +19,11 @@ describe('<NewMessageForm />', () => {
     it('clears the text field', async () => {
       await sendMessage();
       expect(screen.getByTestId('messageText').value).toEqual('');
+    });
+
+    it('calls the send handler', async () => {
+      await sendMessage();
+      expect(sendHandler).toHaveBeenCalledWith('New message');
     });
   });
 });
