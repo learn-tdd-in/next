87599af18b85ee4df60670aa75c5b42da6433ba4
Josh Justice <jjustice@justworks.com>
2022-06-15 12:44:51 -0400

    Add message list

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import {useState} from 'react';
+import MessageList from '../components/MessageList';
 import NewMessageForm from '../components/NewMessageForm';
 
 export default function Home() {
-  function handleSend() {}
+  const [messages, setMessages] = useState([]);
+  function handleSend(newMessage) {
+    setMessages([newMessage, ...messages]);
+  }
 
-  return <NewMessageForm onSend={handleSend} />;
+  return (
+    <>
+      <NewMessageForm onSend={handleSend} />
+      <MessageList data={messages} />
+    </>
+  );
 }
